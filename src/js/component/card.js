e89b53beca1fa12fe887636a9881f51effee1b15
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-04-03 05:37:03 +0000

    avances de correciones del card hasta el momento

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,14 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 
-const Card = () => {
+const Card = (props) => {
+    console.log(props);
     return (
-        <div className="container">
-            <h1>auqui hay que colocar el card</h1>
+        <div className="col-md-3 ps-0">
+            <div className="card">
+                <img src={props.ruta} className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title">{props.nombre}</h5>
+                    <p className="card-text">{props.descripcion}</p>
+                </div>
+                <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item">
+                        <a href="#" className="btn btn-primary">Find Our More!</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-
     );
 };
 
+Card.propTypes = {
+    nombre: PropTypes.string,
+    descripcion: PropTypes.string
+}
 
 export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,16 +7,28 @@ import JumboTrom from "../jumbotron";
 import Card from "./card";
 
 
+const datos = [{ nombre: "irene zoe", descripcion: "Como escritora y cineasta se doctoró en Literatura por la Universidad de Columbia", imagen: "https://e00-elmundo.uecdn.es/elmundo/imagenes/2013/01/29/espana/1359456471_0.jpg" },
+{ nombre: "Julieta Gracián", descripcion: "conocida como Julieta, es una cantante y compositora española", imagen: "https://i.pinimg.com/originals/6d/90/fa/6d90fa0a7448dc6f14918aa2122c8627.png" },
+{ nombre: "Cai Xukun", descripcion: "es un popular cantante, actor, rapero, bailarín y compositor chino", imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Cai_Xukun%2C_28_November_2018_%284%29.jpg/200px-Cai_Xukun%2C_28_November_2018_%284%29.jpg" },
+{ nombre: "Marco Borsato", descripcion: "Comenzó a cantar en italiano, pero en 1994 cambió al idioma neerlandés.", imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Marco_Borsato_2016.tif/lossy-page1-1920px-Marco_Borsato_2016.tif.jpg" }
+];
+
 
 //create your first component
 const Home = () => {
 	return (
 		<>
 			<NavBar />
-			<JumboTrom />
-			<Card />
-
-
+			<div className="container">
+				<div className="row">
+					<JumboTrom />
+				</div>
+				<div className="row">
+					{datos.map((value, index) => {
+						return <Card key={index} nombre={value.nombre} descripcion={value.descripcion} ruta={value.imagen} />
+					})}
+				</div>
+			</div>
 
 
 		</>
